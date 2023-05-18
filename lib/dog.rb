9671d68d087a4183dc180c5f4f39f919c6f77c27
edd-ie <ogendoeddie@gmail.com>
2023-05-18 14:33:41 +0300

    complete

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog
+    def name=(value)
+        @this_dogs_name = value
+    end
 
+    # getter method
+    def name
+        # access the data from the @name instance variable and return it
+        @this_dogs_name
+    end
 end
+
+# c = Dog.new
+# c.name = "Lassie"
+# puts c.name
